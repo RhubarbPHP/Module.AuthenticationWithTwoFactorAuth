bc9cdd276c095cb3c998166040d448faa64863b1
Catherine Doherty <catherine@gcdtech.com>
2017-10-10 15:25:55 +0100

    beginning of TOTP verification

--- src/Login/TwoFactorLogin.php
+++ src/Login/TwoFactorLogin.php
@@ -2,12 +2,49 @@
 
 namespace Rhubarb\AuthenticationWithTwoFactorAuth\Login;
 
+use Rhubarb\AuthenticationWithTwoFactorAuth\LoginProviders\TwoFactorLoginProvider;
 use Rhubarb\Crown\Exceptions\ForceResponseException;
+use Rhubarb\Crown\LoginProviders\Exceptions\LoginDisabledException;
+use Rhubarb\Crown\LoginProviders\Exceptions\LoginFailedException;
+use Rhubarb\Crown\Request\Request;
+use Rhubarb\Crown\Request\WebRequest;
 use Rhubarb\Crown\Response\RedirectResponse;
 use Rhubarb\Scaffolds\Authentication\Leaves\Login;
 
 class TwoFactorLogin extends Login
 {
+    protected function onModelCreated()
+    {
+        /** @var WebRequest $request */
+        $request = Request::current();
+        $redirectUrl = $request->get('rd');
+        if ($redirectUrl) {
+            $redirectUrl = urldecode($redirectUrl);
+            $this->model->redirectUrl = $redirectUrl;
+        }
+
+        $this->model->attemptLoginEvent->attachHandler(function () {
+            $login = TwoFactorLoginProvider::singleton();
+
+            try {
+                if ($login->login($this->model->username, $this->model->password)) {
+
+                    if ($this->model->rememberMe) {
+                        $login->rememberLogin();
+                    }
+
+                    $login->sendTotpCode();
+                    $this->onSuccess();
+                }
+            } catch (LoginDisabledException $er) {
+                $this->model->disabled = true;
+                $this->model->failed = true;
+            } catch (LoginFailedException $er) {
+                $this->model->failed = true;
+            }
+        });
+    }
+
     protected function onSuccess()
     {
         throw new ForceResponseException(new RedirectResponse('twoFactorAuth/'));
--- src/LoginProviders/TwoFactorLoginProvider.php
+++ src/LoginProviders/TwoFactorLoginProvider.php
@@ -2,22 +2,41 @@
 
 namespace Rhubarb\AuthenticationWithTwoFactorAuth\LoginProviders;
 
+use Aws\Sms\SmsClient;
+use OTPHP\TOTP;
+use Rhubarb\AuthenticationWithTwoFactorAuth\User;
+use Rhubarb\AwsSnsSmsProvider\SMSProviders\AwsSnsSmsProvider;
 use Rhubarb\Scaffolds\Authentication\LoginProviders\LoginProvider;
 
 class TwoFactorLoginProvider extends LoginProvider
 {
+    /** @var User */
+    private $user;
+
+    /** @var TOTP */
+    private $totp;
+
     public function isLoggedIn()
     {
+        if (parent::isLoggedIn()) {
+            $this->user = $this->getLoggedInUser();
+        }
+
         // if on 2 factor page, then don't add a check for token was validated, otherwise, do
 
-        //return false;
         return parent::isLoggedIn();
     }
 
-    public function validateTwoFactorInput($token)
+    public function validateTwoFactorInput($input)
     {
         // set token validated true
-
+        if ($this->totp->verify($input)) {
+            $this->loggedIn = true;
+            $this->loggedInUserIdentifier = $this->user->getUniqueIdentifier();
+        } else {
+            $this->loggedIn = false;
+        }
+        $this->storeSession();
     }
 
     public function hasValidToken()
@@ -25,4 +44,10 @@ class TwoFactorLoginProvider extends LoginProvider
         // check session for valid token $this->data
     }
 
+    public function sendTotpCode()
+    {
+        $this->totp = TOTP::create($this->user->TFASecret, 30, 'sha256');
+        $sms = new AwsSnsSmsProvider();
+        $sms->send();
+    }
 }
